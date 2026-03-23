d2a1b68facbdd7718e11b1261c9ef757353fdfc7
Johnbulongdon <126181192+Johnbulongdon@users.noreply.github.com>
2026-03-23 11:54:56 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -54,5 +54,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/profile/:path*', '/settings/:path*', '/login'],
+  matcher: ['/profile/:path*', '/settings/:path*', '/login'],
 }
